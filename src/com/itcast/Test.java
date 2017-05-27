2fd5209a4ced2b4bea848a26f7f8c8a252f61936
gjn828 <1187190115@qq.com>
2017-05-27 09:33:33 +0800

    Initial commit

---
+++ src/com/itcast/Test.java
@@ -0,0 +1,21 @@
+package com.itcast;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+public class Test {
+    public static void main(String[] args) {
+        double aa = Math.random();
+        System.out.println("haha");
+
+        for (int i = 0; i < 100000; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+
+
+
+
+}
